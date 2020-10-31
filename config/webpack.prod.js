227d7d26b63c1b8ba0830747d0661c8aa2171c8d
Andres Bedoya <mortiis.angel@gmail.com>
2020-10-30 22:19:00 -0500

    css optimization plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,33 @@
 const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+
+  devtool: 'source-map',
+
+  optimization: {
+    minimize: true
+  },
+
+  performance: {
+    hints: false,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    })
+  ],
 });
